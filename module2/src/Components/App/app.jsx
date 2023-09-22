739c88e856d239c3c1a78d3fa1ccdbbe2593ad71
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-09-22 12:32:16 +0200

    module2 : events

--- module2/src/Components/App/app.jsx
+++ module2/src/Components/App/app.jsx
@@ -7,31 +7,21 @@ const App = () => {
   
     console.log('rendering with counter value', counter)
   
-    const increaseByOne = () => {
-  
-      console.log('increasing, value before', counter)
-      setCounter(counter + 1)
-    }
-  
-    const decreaseByOne = () => { 
-  
-      console.log('decreasing, value before', counter)
-      setCounter(counter - 1)
-    }
-  
-    const setToZero = () => {
-  
-      console.log('resetting to zero, value before', counter)
-      setCounter(0)
+    const  changeCount = (delta) => {
+        setCounter(counter + delta);
     }
   
     return (
       <div>
         <Display counter={counter} />
-        <Button handleClick={increaseByOne} text="plus" />
-        <Button handleClick={setToZero} text="zero" />
-        <Button handleClick={decreaseByOne} text="minus" />
+        <Button changeCount={changeCount} text="plus" delta={1} />
+        <Button changeCount={changeCount} text="zero" delta={-counter}/>
+        <Button changeCount={changeCount} text="minus" delta={-1} />
       </div>
     )
   } 
+
+
+ 
+
 export default App
