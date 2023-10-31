b0b829a31eeb8ba18df93846627b3dcdc5b2a089
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-10-31 17:01:21 +0100

    module 4 : 2.7 PhoneBook Step2

--- module4/part1/src/App.jsx
+++ module4/part1/src/App.jsx
@@ -6,19 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson} >
         <div>
-          name: <input />
+          name: <input type="text" value={newName} onChange={handleName} />
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+            <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-     
+      <div>
+        {persons.map((person,i ) => 
+        <li key= {i} >
+          {person.name}
+        </li> )}
+      </div>
     </div>
   )
 }
