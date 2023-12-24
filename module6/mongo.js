79f8052395ba3b5a43631181411348c80c8f8a84
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-12-24 21:59:12 +0100

    update 3.12 and start 3.13-3.14

--- module6/mongo.js
+++ module6/mongo.js
@@ -1,70 +1,69 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-if (process.argv.length<3) {
-  console.log('Usage :');
-  console.log('GET: node mongo.js [password]');
-  console.log('POST: node monge.js [password] [name] [number]');
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log('Operations :  ');
+  console.log('1 - GET: " node mongo.js [password] "');
+  console.log('2 - POST: " node monge.js [password] [name] [number] "');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://dorcas:${password}@cluster0.auznpxq.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://dorcas:${password}@cluster0.auznpxq.mongodb.net/?retryWrites=true&w=majority`;
 
-  mongoose.set('strictQuery',false)
-  
-  async function main() {
-    try {
-      await mongoose.connect(url);
-   
-  
-      const phoneBookSchema = new mongoose.Schema({
-        nom: String,
-        number: String,
-      });
-  
-      const Phonebook = mongoose.model('Phonebook', phoneBookSchema);
-  
+mongoose.set('strictQuery', false);
+
+async function main() {
+  let connection;
+  try {
+    connection = await mongoose.connect(url);
+    console.log('connected');
+
+    const phoneBookSchema = new mongoose.Schema({
+      nom: String,
+      number: String,
+    });
+
+    const Phonebook = mongoose.model('Phonebook', phoneBookSchema);
+
+    /**
+     * Opération de recherche
+     * node mongo.js password
+     */
 
     if (process.argv.length === 3) {
-    
-    /**
-     * Opération de recherche 
-     * node mongo.js password 
-     */
-    Phonebook.find({}).then(result => {
-      console.log("phoneBook : ");
-      result.forEach(phone => {
+      const result = await Phonebook.find({});
+      console.log('phoneBook : ');
+      result.forEach((phone) => {
         console.log(`${phone.nom} ${phone.number}`);
-      })
-      mongoose.connection.close();
-      process.exit(0);
-    });
-  }
+      });
+    }
 
-  /**
-     * Opération de recherche 
+    /**
+     * Opération d'ajout
      * node mongo.js password name phoneNumber
-  */
+     */
 
-  if (process.argv.length === 5) {
-    const nom = process.argv[3];
-    const number = process.argv[4];
+    if (process.argv.length === 5) {
+      const nom = process.argv[3];
+      const number = process.argv[4];
 
-    const phonebookMember = new Phonebook({
-      nom: nom,
-      number: number,
-    });
+      const phonebookMember = new Phonebook({
+        nom: nom,
+        number: number,
+      });
 
-    await phonebookMember.save();
-    console.log(`Added ${nom} with number ${number} to phonebook`);
+      await phonebookMember.save();
+      console.log(`Added ${nom} with number ${number} to phonebook`);
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    if (connection) {
+      await connection.disconnect();
+      console.log('disconnected');
+    }
   }
-} catch (err) {
-  console.error('Error:', err);
-} finally {
-  mongoose.connection.close();
-}
 }
 
 main();
